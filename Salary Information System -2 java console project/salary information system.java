136c6d5c2cb12a49b9a48824605e4704f71cf2d5
bhanuka viduranga <bhanukav10@gmail.com>
2025-08-02 19:43:56 +0530

    Update salary information system.java

--- Salary Information System -2 java console project/salary information system.java
+++ Salary Information System -2 java console project/salary information system.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-class Assignment04 {
+class salaryinformationsystem {
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         System.out.println("---------------------------------------------------------------------------");
